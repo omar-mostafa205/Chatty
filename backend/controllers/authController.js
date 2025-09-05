53f3fe58dfe208a04313715c4f0b19ece57d3ccc
Omar Mostafa <omar.mostafa0205@gmail.com>
2025-09-05 10:29:42 +0300

    add logina and me functionality in our auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 import User from "../models/User.js"
 import generateUniqueConnectCode from "../utils/generateUniqueConnectCode.js";
+
 
 class AuthController {
     static async register(req, res) {
@@ -42,6 +44,77 @@ class AuthController {
 
         } catch (error) {
             console.error("Registration error", error);
+            res.status(500).json({message: "Internal server error"});
+        }
+    }
+
+    static async login(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({message: "Invalid credentials"});
+            }
+
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({message: "Invalid credentials"});
+            }
+
+            const isValidPassword = await bcrypt.compare(password, user.password);
+            if (!isValidPassword) {
+                return res.status(400).json({message: "Invalid credentials"});
+            }
+
+            const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {
+                expiresIn: '7d'
+            })
+
+            res.cookie("jwt", token, {
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: 'strict',
+                secure: process.env.NODE_ENV !== "development"
+            })
+
+            res.status(200).json({
+                user: {
+                    id: user.id,
+                    username: user.username,
+                    fullName: user.fullName,
+                    email: user.email,
+                    connectCode: user.connectCode,
+                }
+            })
+
+
+        } catch (error) {
+            console.error("Login error", error);
+            res.status(500).json({message: "Internal server error"});
+        }
+    }
+
+    static async me(req, res) {
+        try {
+            const user = await User.findById(req.user.id).select("-password");
+
+            if (!user) {
+                return res.status(400).json({message: "User not found"});
+            }
+
+            res.status(200).json({
+                user: {
+                    id: user.id,
+                    username: user.username,
+                    fullName: user.fullName,
+                    email: user.email,
+                    connectCode: user.connectCode,
+                }
+            })
+
+        } catch (error) {
+            console.error("Me error", error);
             res.status(500).json({message: "Internal server error"});
         }
     }
