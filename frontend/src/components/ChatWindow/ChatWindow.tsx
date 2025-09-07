bf2e2298197d9239263886056d72da18a71b20b0
Omar Mostafa <omar.mostafa0205@gmail.com>
2025-09-08 01:05:22 +0300

    work on the chat page UI

---
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -0,0 +1,9 @@
+import ChatPlaceholder from "./ChatPlaceholder";
+
+const ChatWindow: React.FC = () => {
+    return <div className="min-h-screen w-full bg-white flex flex-col justify-between">
+        <ChatPlaceholder />
+    </div>
+}
+
+export default ChatWindow;
