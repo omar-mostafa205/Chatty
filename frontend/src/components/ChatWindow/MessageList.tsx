e688d0eee572272c729ff4214985cfded55b5b4e
Omar Mostafa <omar.mostafa0205@gmail.com>
2025-09-26 17:53:44 +0300

    scroll messages container to bottom on conversation change

--- frontend/src/components/ChatWindow/MessageList.tsx
+++ frontend/src/components/ChatWindow/MessageList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useMessages } from "../../hooks/useMessages";
 import { useConversationStore } from "../../stores/conversationStore";
 import MessageItem from "./MessageItem";
@@ -5,8 +6,22 @@ import MessageItem from "./MessageItem";
 const MessageList: React.FC = () => {
     const { selectedConversation } = useConversationStore();
     const {data, isLoading} = useMessages(selectedConversation?.conversationId);
+    const containerRef = useRef<HTMLDivElement | null>(null);
 
     const allMessages = data?.pages.slice().reverse().flatMap((page) => page.messages) ?? [];
+
+    useEffect(() => {
+        if (!selectedConversation?.conversationId) return;
+
+        if (data?.pages.length == 1) {
+            setTimeout(() => {
+                if (containerRef.current) {
+                    containerRef.current.scrollTop = containerRef.current.scrollHeight;
+                }
+            }, 0)
+        }
+
+    }, [data, selectedConversation])
 
     if (isLoading) {
         return <div className="relative flex-1 h-full flex items-center justify-center">
@@ -14,7 +29,7 @@ const MessageList: React.FC = () => {
         </div>
     }    
 
-    return <div className="flex-1 bg-gray-50 overflow-y-auto p-4 pb-10">
+    return <div ref={containerRef} className="flex-1 bg-gray-50 overflow-y-auto p-4 pb-10">
         {allMessages.map((message) => (
             <div key={message._id}>
                 <MessageItem {...message}/>
